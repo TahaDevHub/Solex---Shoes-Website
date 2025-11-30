0a3bc05d61cbe7defd8d7827d2cf518e671c7b0d
TahaDevHub <muhammadtaha.developer@gmail.com>
2025-12-01 01:59:59 +0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,18 @@
 // ======= Script Contact Form =======
-document.querySelector(".contactformdiv form button").addEventListener("click", (e)=>{
-    e.preventDefault();
-    alert("Thank you for contacting us... We will respond to you very soon.");
 
-});
+const submitBtn=document.getElementById('submitBtn');
+submitBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const fname=document.getElementById("fname").value.trim();; //.value.trim() gives the actual text typed by the user. ,,,,is used to retrieve the value of an input element and then remove any leading or trailing whitespace from that value.
+    const phoneno=document.getElementById("phonenumber").value.trim();;
+    const email=document.getElementById("email").value.trim();;
+
+    if(fname==="" || phoneno==="" || email===""){
+        alert("All the fields are required. Please input each detail.");
+        return;
+    }
+    
+    alert("Thank you for contacting us... We will respond to you very soon.");
+    
+
+})

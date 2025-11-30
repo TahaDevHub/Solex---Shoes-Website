89fcf9ac16d3451e0f83d03496289c64135652b0
TahaDevHub <muhammadtaha.developer@gmail.com>
2025-11-30 15:30:33 +0500

    Fix event listener selector for contact form button

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,6 @@
 // ======= Script Contact Form =======
-document.querySelector(".contactformdiv form").addEventListener("click", (e)=>{
+document.querySelector(".contactformdiv form button").addEventListener("click", (e)=>{
     e.preventDefault();
     alert("Thank you for contacting us... We will respond to you very soon.");
+
 });

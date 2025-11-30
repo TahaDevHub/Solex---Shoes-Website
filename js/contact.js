e58095b27331ccec68f49cd6b819de7dc119d4bd
TahaDevHub <muhammadtaha.developer@gmail.com>
2025-12-01 02:12:59 +0500

    Refactor contact form submission handling

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,5 @@
 // ======= Script Contact Form =======
-
-const submitBtn=document.getElementById('submitBtn');
-submitBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const fname=document.getElementById("fname").value.trim();; //.value.trim() gives the actual text typed by the user. ,,,,is used to retrieve the value of an input element and then remove any leading or trailing whitespace from that value.
-    const phoneno=document.getElementById("phonenumber").value.trim();;
-    const email=document.getElementById("email").value.trim();;
-
-    if(fname==="" || phoneno==="" || email===""){
-        alert("All the fields are required. Please input each detail.");
-        return;
-    }
-    
+const form=document.querySelector('form');
+form.addEventListener("submit", (e)=>{
     alert("Thank you for contacting us... We will respond to you very soon.");
-    
-
-})
+});

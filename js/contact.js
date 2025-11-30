75dc52a70e4ac1ed4337a8562ba5b0a38e150b5b
KashifaM07 <kabsha2007@gmail.com>
2025-11-30 15:11:21 +0500

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+// ======= Script Contact Form =======
+document.querySelector(".contactformdiv form").addEventListener("click", (e)=>{
+    e.preventDefault();
+    alert("Thank you for contacting us... We will respond to you very soon.");
+});
